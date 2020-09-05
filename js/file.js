86f60ff857aadc17aac683de5b3b30d7e4bd7224
Vad11m-vek <55487186+Vad11m-vek@users.noreply.github.com>
2020-09-05 12:38:01 +0300

    first push

---
+++ js/file.js
@@ -0,0 +1,52 @@
+let $nameOfTheLegalEntity = document.querySelector('#nameOfTheLegalEntity').value;
+let $TINorUSREOU = document.querySelector('#TINorUSREOU').value;
+let $Location = document.querySelector('#Location').value;
+let $tel = document.querySelector('#tel').value;
+let $emailUser = document.querySelector('#emailUser').value;
+
+let doc = new jsPDF();
+
+doc.setFontSize(12);
+
+
+doc.addFont("font/PTSans.ttf", "PTSans", "normal");
+doc.setFont("PTSans");
+doc.text("Бланк претензії для – юридичних осіб та ФОП", 110, 15);
+doc.text('ТОВ «Нова Пошта»', 110, 20);
+//out name
+let strNameOfTheLegalEntity = doc.splitTextToSize('' + $nameOfTheLegalEntity, 100)
+doc.text(strNameOfTheLegalEntity, 110, 30);
+//out ІПН або ЄДРПОУ
+doc.text("ІПН або ЄДРПОУ", 110, 40);
+let strTINorUSREOU = doc.splitTextToSize('' + $TINorUSREOU, 50)
+doc.text(strTINorUSREOU, 150, 40);
+//out Місцезнаходження
+doc.text("Місцезнаходження :", 110, 45);
+let strLocation = doc.splitTextToSize('' + $Location, 100)
+doc.text(strLocation, 110, 50);
+//out number phone
+doc.text("Моб. тел. :", 110, 65);
+let strtel = doc.splitTextToSize('' + $tel, 50)
+doc.text(strtel, 150, 65);
+//out email
+doc.text("Ел. пошта :", 110, 70);
+let strEmailUser = doc.splitTextToSize('' + $emailUser, 50)
+doc.text(strEmailUser, 150, 70);
+//Declare
+doc.setFontSize(20);
+doc.text("ПРЕТЕНЗІЯ", 80, 80);
+doc.setFontSize(14);
+doc.text("У зв’язку з ", 10, 90);
+
+// var strArr = doc.splitTextToSize("A longer title that might be split", 50)
+// doc.text(strArr, 50, 50);
+
+start.addEventListener('click', start => {
+	var string = doc.output('datauristring');
+	var embed = "<embed width='100%' height='100%' src='" + string + "' />"
+	var x = window.open();
+	x.document.open();
+	x.document.write(embed);
+	x.document.close();
+	// doc.save('hello_world.pdf')
+});
