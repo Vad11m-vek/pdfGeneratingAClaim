999dc3a7a57ad6c54b0c72def2161d51d850530c
Vad11m-vek <55487186+Vad11m-vek@users.noreply.github.com>
2020-09-13 21:03:58 +0300

    add localStorage img and cookie - text

--- js/file.js
+++ js/file.js
@@ -1,27 +1,27 @@
-let $nameOfTheLegalEntity = document.querySelector('#nameOfTheLegalEntity').value;
-let $TINorUSREOU = document.querySelector('#TINorUSREOU').value;
-let $Location = document.querySelector('#Location').value;
-let $tel = document.querySelector('#tel').value;
-let $emailUser = document.querySelector('#emailUser').value;
+let $nameOfTheLegalEntity = document.querySelector('#nameOfTheLegalEntity');
+let $TINorUSREOU = document.querySelector('#TINorUSREOU');
+let $Location = document.querySelector('#Location');
+let $tel = document.querySelector('#tel');
+let $emailUser = document.querySelector('#emailUser');
 let $defectCollection = document.querySelectorAll('[name="defect"]');
 let $reimburseCollection = document.querySelectorAll('[name="reimburse"]');
 let $cancellationCollection = document.querySelectorAll('[name="cancellation"]');
 let defectReasonCollection = '';
 let reimburseReasonCollection = '';
 let cancellationReasonCollection = '';
-let $invoiceNumber = document.querySelector('#invoiceNumber').value;
-let $price = document.querySelector('#price').value;
-let $otherProblem = document.querySelector('#otherProblem').value;
-let $theEssenceOfTheProblem = document.querySelector('#theEssenceOfTheProblem').value;
-let $nameOfThePerson = document.querySelector('#nameOfThePerson').value;
-let $identificationCode = document.querySelector('#identificationCode').value;
-let $currentAccount = document.querySelector('#currentAccount').value;
-let $MFIBank = document.querySelector('#MFIBank').value;
+let $invoiceNumber = document.querySelector('#invoiceNumber');
+let $price = document.querySelector('#price');
+let $otherProblem = document.querySelector('#otherProblem');
+let $theEssenceOfTheProblem = document.querySelector('#theEssenceOfTheProblem');
+let $nameOfThePerson = document.querySelector('#nameOfThePerson');
+let $identificationCode = document.querySelector('#identificationCode');
+let $currentAccount = document.querySelector('#currentAccount');
+let $MFIBank = document.querySelector('#MFIBank');
 let $copyOfTheAct = document.querySelector('#copyOfTheAct');
 let $dateCopyOfTheAct = document.querySelector('#dateCopyOfTheAct').value = moment().format('YYYY-MM-DD');
 let $aCopyOfTheDocumentConfirmingTheCostOfSending = document.querySelector('#aCopyOfTheDocumentConfirmingTheCostOfSending');
 let $dateDoc = document.querySelector('#dateDoc').value = moment().format('YYYY-MM-DD');
-let $otherAdditions = document.querySelector('#otherAdditions').value;
+let $otherAdditions = document.querySelector('#otherAdditions');
 let $clientSignature = document.querySelector('#clientSignature');
 let urlClientSignature;
 let claimDetails = {
@@ -36,51 +36,38 @@ let claimDetails = {
 	MFIBank: '',
 	clientSignature: '',
 };
-document.querySelector('h3').addEventListener('click', () => {
-	let claimDetailCookie = JSON.parse(getCookie('claimDetail'));
-	console.log(claimDetailCookie);
-	$nameOfTheLegalEntity = claimDetailCookie.nameOfTheLegalEntity;
-	$TINorUSREOU = claimDetailCookie.TINorUSREOU;
-	$Location = claimDetailCookie.Location;
-	$tel = claimDetailCookie.tel;
-	$emailUser = claimDetailCookie.emailUser;
-	$nameOfThePerson = claimDetailCookie.nameOfThePerson;
-	$identificationCode = claimDetailCookie.identificationCode;
-	$currentAccount = claimDetailCookie.currentAccount;
-	$MFIBank = claimDetailCookie.MFIBank;
-	$clientSignature.value = claimDetailCookie.clientSignature;
-})
-// let claimDetail = 'claimDetail';
 //check url clientSignature
-$clientSignature.addEventListener('change', (event) => {
+$clientSignature.addEventListener('change', event => {
 	let oFReader = new FileReader();
 	oFReader.readAsDataURL(document.querySelector('#clientSignature').files[0]);
 	oFReader.onload = function (oFREvent) {
 		urlClientSignature = oFREvent.target.result;
 	};
 });
-// cancellation
-// document.querySelector('h3').addEventListener('click', () => {
-// 	$price = document.querySelector('#price').value;
-// 	if ($price.length == 0) {
-// 		console.log($price.length);
-// 	} else {
-// 		console.log('veloseped');
-// 	}
-// })
-
+if (getCookie('claimDetail')) {
+	let claimDetailCookie = JSON.parse(getCookie('claimDetail'));
+	$nameOfTheLegalEntity.value = claimDetailCookie.nameOfTheLegalEntity;
+	$TINorUSREOU.value = claimDetailCookie.TINorUSREOU;
+	$Location.value = claimDetailCookie.Location;
+	$tel.value = claimDetailCookie.tel;
+	$emailUser.value = claimDetailCookie.emailUser;
+	$nameOfThePerson.value = claimDetailCookie.nameOfThePerson;
+	$identificationCode.value = claimDetailCookie.identificationCode;
+	$currentAccount.value = claimDetailCookie.currentAccount;
+	$MFIBank.value = claimDetailCookie.MFIBank;
+}
 let doc = new jsPDF();
 function documentWriter() {
 	//get new value
-	$nameOfTheLegalEntity = document.querySelector('#nameOfTheLegalEntity').value;
-	$TINorUSREOU = document.querySelector('#TINorUSREOU').value;
-	$Location = document.querySelector('#Location').value;
-	$tel = document.querySelector('#tel').value;
-	$emailUser = document.querySelector('#emailUser').value;
-	$nameOfThePerson = document.querySelector('#nameOfThePerson').value;
-	$identificationCode = document.querySelector('#identificationCode').value;
-	$currentAccount = document.querySelector('#currentAccount').value;
-	$MFIBank = document.querySelector('#MFIBank').value;
+	$nameOfTheLegalEntity = document.querySelector('#nameOfTheLegalEntity');
+	$TINorUSREOU = document.querySelector('#TINorUSREOU');
+	$Location = document.querySelector('#Location');
+	$tel = document.querySelector('#tel');
+	$emailUser = document.querySelector('#emailUser');
+	$nameOfThePerson = document.querySelector('#nameOfThePerson');
+	$identificationCode = document.querySelector('#identificationCode');
+	$currentAccount = document.querySelector('#currentAccount');
+	$MFIBank = document.querySelector('#MFIBank');
 	$copyOfTheAct = document.querySelector('#copyOfTheAct');
 	//write pdf
 	doc.setFontSize(11);
@@ -89,23 +76,23 @@ function documentWriter() {
 	doc.text("Бланк претензії для – юридичних осіб та ФОП", 110, 15);
 	doc.text('ТОВ «Нова Пошта»', 110, 20);
 	//out name
-	let strNameOfTheLegalEntity = doc.splitTextToSize('' + $nameOfTheLegalEntity, 100)
+	let strNameOfTheLegalEntity = doc.splitTextToSize('' + $nameOfTheLegalEntity.value, 100)
 	doc.text(strNameOfTheLegalEntity, 110, 25);
 	//out ІПН або ЄДРПОУ
 	doc.text("ІПН або ЄДРПОУ", 110, 40);
-	let strTINorUSREOU = doc.splitTextToSize('' + $TINorUSREOU, 50)
+	let strTINorUSREOU = doc.splitTextToSize('' + $TINorUSREOU.value, 50)
 	doc.text(strTINorUSREOU, 150, 40);
 	//out Місцезнаходження
 	doc.text("Місцезнаходження :", 110, 45);
-	let strLocation = doc.splitTextToSize('' + $Location, 100)
+	let strLocation = doc.splitTextToSize('' + $Location.value, 100)
 	doc.text(strLocation, 110, 50);
 	//out number phone
 	doc.text("Моб. тел. :", 110, 65);
-	let strtel = doc.splitTextToSize('' + $tel, 50)
+	let strtel = doc.splitTextToSize('' + $tel.value, 50)
 	doc.text(strtel, 150, 65);
 	//out email
 	doc.text("Ел. пошта :", 110, 70);
-	let strEmailUser = doc.splitTextToSize('' + $emailUser, 50)
+	let strEmailUser = doc.splitTextToSize('' + $emailUser.value, 50)
 	doc.text(strEmailUser, 150, 70);
 	//declare
 	doc.setFontSize(17);
@@ -117,11 +104,11 @@ function documentWriter() {
 	doc.text(defectReason, 30, 90);
 	let textDuringTransportation = doc.splitTextToSize('відправлення, що сталося під час надання послуги з організації його перевезення за експрес-накладною', 200)
 	doc.text(textDuringTransportation, 8, 100);
-	doc.text(`№ ${$invoiceNumber} прошу :`, 8, 105);
+	doc.text(`№ ${$invoiceNumber.value} прошу :`, 8, 105);
 	// nead update price
-	$price = document.querySelector('#price').value;
-	if ($price.length != 0) {
-		doc.text(`відшкодувати вартість відправлення в розмірі ${$price} грн.`, 8, 110);
+	$price = document.querySelector('#price');
+	if ($price.value.length != 0) {
+		doc.text(`відшкодувати вартість відправлення в розмірі ${$price.value} грн.`, 8, 110);
 	}
 	if (reimburseReasonCollection.length != 0) {
 		doc.text(`відшкодувати сплачену вартість${reimburseReasonCollection}`, 8, 115);
@@ -129,63 +116,63 @@ function documentWriter() {
 	if (cancellationReasonCollection.length != 0) {
 		doc.text(`анулювати нараховану плату за${cancellationReasonCollection}`, 8, 120);
 	}
-	$otherProblem = document.querySelector('#otherProblem').value;
-	if ($otherProblem.length != 0) {
+	$otherProblem = document.querySelector('#otherProblem');
+	if ($otherProblem.value.length != 0) {
 		doc.text('інше : ', 8, 125);
-		let strotherProblem = doc.splitTextToSize('' + $otherProblem, 190)
+		let strotherProblem = doc.splitTextToSize('' + $otherProblem.value, 190)
 		doc.text(strotherProblem, 21, 125);
 	}
 	//about problem
 	doc.text('Суть проблеми / опис відправлення: ', 8, 135);
-	let strTheEssenceOfTheProblem = doc.splitTextToSize('' + $theEssenceOfTheProblem, 200)
+	let strTheEssenceOfTheProblem = doc.splitTextToSize('' + $theEssenceOfTheProblem.value, 200)
 	doc.text(strTheEssenceOfTheProblem, 8, 140);
 	let textCommentDetails = doc.splitTextToSize('У випадку прийняття рішення щодо оплати суми передбаченої цією претензією (або її частини), зазначені кошти прошу перерахувати на наступні реквізити', 200)
 	doc.text(textCommentDetails, 8, 200);
 	// requisites left column
 	doc.text("Найменування юридичної особи / ФОП", 5, 215);
-	let textNameOfThePerson = doc.splitTextToSize('' + $nameOfThePerson, 110)
+	let textNameOfThePerson = doc.splitTextToSize('' + $nameOfThePerson.value, 110)
 	doc.text(textNameOfThePerson, 5, 220);
 	doc.text('Ідентифікаційний код юридичної особи або ІПН ФОП', 5, 230);
-	doc.text(`${$identificationCode}`, 5, 235);
+	doc.text(`${$identificationCode.value}`, 5, 235);
 	//signature img
-	doc.addImage(`${urlClientSignature}`, "JPEG", 120, 220, 46, 34);
-	doc.addImage(`${urlClientSignature}`, "JPEG", 120, 260, 46, 34);
+	doc.addImage(`${img.src}`, "JPEG", 120, 220, 46, 34);
+	doc.addImage(`${img.src}`, "JPEG", 120, 260, 46, 34);
 	// requisites right column
 	doc.text("Розрахунковий рахунок", 110, 215);
-	doc.text(`${$currentAccount}`, 110, 220);
+	doc.text(`${$currentAccount.value}`, 110, 220);
 	doc.text("МФО банку : ", 110, 225);
-	doc.text(`${$MFIBank}`, 130, 225);
+	doc.text(`${$MFIBank.value}`, 130, 225);
 	//Additions Додатки:
 	if ($copyOfTheAct.checked || $aCopyOfTheDocumentConfirmingTheCostOfSending.checked || ($otherAdditions.value != 0)) {
 		doc.text("Додатки : ", 5, 245);
 		if ($copyOfTheAct.checked) {
-			$dateCopyOfTheAct = document.querySelector('#dateCopyOfTheAct').value;
-			doc.text(`Копія Акта приймання-передачі від ${reverseValueDate($dateCopyOfTheAct)}р., складеного з представником ТОВ «Нова Пошта».`, 5, 250);
+			$dateCopyOfTheAct = document.querySelector('#dateCopyOfTheAct');
+			doc.text(`Копія Акта приймання-передачі від ${reverseValueDate($dateCopyOfTheAct.value)}р., складеного з представником ТОВ «Нова Пошта».`, 5, 250);
 		}
 		if ($aCopyOfTheDocumentConfirmingTheCostOfSending.checked) {
 			doc.text('Копія документа, який підтверджує вартість відправлення.', 5, 255);
 		}
-		$otherAdditions = document.querySelector('#otherAdditions').value;
+		$otherAdditions = document.querySelector('#otherAdditions');
 		if ($otherAdditions != 0) {
 			doc.text('інше : ', 5, 260);
-			doc.text(`${$otherAdditions}`, 15, 260);
+			doc.text(`${$otherAdditions.value}`, 15, 260);
 		}
 	}
-	$dateDoc = document.querySelector('#dateDoc').value;
-	doc.text(`Дата  ${reverseValueDate($dateDoc)}р.`, 5, 290);
+	$dateDoc = document.querySelector('#dateDoc');
+	doc.text(`Дата  ${reverseValueDate($dateDoc.value)}р.`, 5, 290);
 	//signature str
 	doc.text('Підпис Клієнта __________________', 110, 240);
 	doc.text('Підпис Клієнта __________________', 110, 282);
 	//record Json
-	claimDetails.nameOfTheLegalEntity = $nameOfTheLegalEntity;
-	claimDetails.TINorUSREOU = $TINorUSREOU;
-	claimDetails.Location = $Location;
-	claimDetails.tel = $tel;
-	claimDetails.emailUser = $emailUser;
-	claimDetails.nameOfThePerson = $nameOfThePerson;
-	claimDetails.identificationCode = $identificationCode;
-	claimDetails.currentAccount = $currentAccount;
-	claimDetails.MFIBank = $MFIBank;
+	claimDetails.nameOfTheLegalEntity = $nameOfTheLegalEntity.value;
+	claimDetails.TINorUSREOU = $TINorUSREOU.value;
+	claimDetails.Location = $Location.value;
+	claimDetails.tel = $tel.value;
+	claimDetails.emailUser = $emailUser.value;
+	claimDetails.nameOfThePerson = $nameOfThePerson.value;
+	claimDetails.identificationCode = $identificationCode.value;
+	claimDetails.currentAccount = $currentAccount.value;
+	claimDetails.MFIBank = $MFIBank.value;
 	claimDetails.clientSignature = $clientSignature.value;
 	setCookie('claimDetail', JSON.stringify(claimDetails), 360);
 }
@@ -282,3 +269,86 @@ function getCookie(name) {
 	}
 	return null;
 }
+//img save test
+// Get all variables
+var bannerImage = document.getElementById('clientSignature');
+var result = document.getElementById('res');
+var img = document.getElementById('tableBanner');
+
+// 
+bannerImage.addEventListener('change', function () {
+	var file = this.files[0];
+	// declare a maxSize (3Mb)
+	var maxSize = 3000000;
+
+	if (file.type.indexOf('image') < 0) {
+		res.innerHTML = 'invalid type';
+		return;
+	}
+	var fReader = new FileReader();
+	fReader.onload = function () {
+		img.onload = function () {
+			// if localStorage fails, it should throw an exception
+			try {
+				// pass the ratio of the file size/maxSize to your toB64 func in case we're already out of scope
+				localStorage.setItem("imgData", getBase64Image(img, (file.size / maxSize), file.type));
+			}
+			catch (e) {
+				var msg = e.message.toLowerCase();
+				// We exceeded the localStorage quota
+				if (msg.indexOf('storage') > -1 || msg.indexOf('quota') > -1) {
+					// we're dealing with a jpeg image :  try to reduce the quality
+					if (file.type.match(/jpe?g/)) {
+						console.log('reducing jpeg quality');
+						localStorage.setItem("imgData", getBase64Image(img, (file.size / maxSize), file.type, 0.7));
+					}
+					// we're dealing with a png image :  try to reduce the size
+					else {
+						console.log('reducing png size');
+						// maxSize is a total approximation I got from some tests with a random pixel generated img
+						var maxPxSize = 750000,
+							imgSize = (img.width * img.height);
+						localStorage.setItem("imgData", getBase64Image(img, (imgSize / maxPxSize), file.type));
+					}
+				}
+			}
+		}
+		img.src = fReader.result;
+	};
+
+	fReader.readAsDataURL(file);
+});
+
+function getBase64Image(img, sizeRatio, type, quality) {
+	// if we've got an svg, don't convert it, svg will certainly be less big than any pixel image
+	if (type.indexOf('svg+xml') > 0) return img.src;
+
+	// if we've got a jpeg
+	if (type.match(/jpe?g/)) {
+		// and the sizeRatio is okay, don't convert it
+		if (sizeRatio <= 1) return img.src;
+	}
+	// if we've got some other image type
+	else type = 'image/png';
+
+	if (!quality) quality = 1;
+	var canvas = document.createElement("canvas");
+	// if our image file is too large, then reduce its size
+	canvas.width = (sizeRatio > 1) ? (img.width / sizeRatio) : img.width;
+	canvas.height = (sizeRatio > 1) ? (img.height / sizeRatio) : img.height;
+
+	var ctx = canvas.getContext("2d");
+	ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+	// if we already tried to reduce its size but it's still failing, then reduce the jpeg quality
+	var dataURL = canvas.toDataURL(type, quality);
+
+	return dataURL;
+}
+
+function fetchimage() {
+	var dataImage = localStorage.getItem('imgData');
+	img.src = dataImage;
+}
+
+// Call fetch to get image from localStorage.
+fetchimage();
